ef3101271e31ad763f17cc578ee8df2d448148f1
Michael Ross <mrosstech@gmail.com>
2022-02-11 20:05:24 -0500

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './components/albums/albums.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AlbumsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
